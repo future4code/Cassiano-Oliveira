56bcb8ac7342ed1dc17e1c834c7cebdb3e6c561b
Cassiano Moura de Oliveira <cassiano980@gmail.com>
2021-08-10 16:42:32 -0300

    Adicinadas algumas pastas

---
+++ semana1/Aula-2/Operacoes/index.js
@@ -0,0 +1,110 @@
+// ----------------------> Exemplos <---------------------------
+/*
+const estouComFome = true
+const estouEmCasa = true
+const estouTriste = true
+
+const vouComerChocolate = estouComFome || estouEmCasa || estouTriste
+
+console.log('Vou comer chocolate?', vou ComerChocolate)
+*/
+/*
+console.log('Olá, Maryam!')
+
+const valor1 = 10
+const valor2 = 20
+const valor3 = 40
+const valor4 = 80
+const valor5 = 3
+
+let resultado
+
+resultado = valor1 + valor2
+
+console.log('Resultado soma: ', resultado)
+
+resultado = valor4 - valor3
+
+console.log('Resultado Substração: ', resultado)
+
+resultado = valor1 * valor3
+
+console.log('Resultado multiplicação: ', resultado)
+
+resultado = valor2 / valor5
+
+const primeiroValor = 345
+const segundoValor = 10
+
+const resultado = primeiroValor / segundoValor
+console.log(resultado)
+*/
+
+
+// ----------------------> Exercicio 1 <---------------------------
+// ----------------------> Execicio 2 <---------------------------
+//Crie duas variáveis que guardem dois números. Imprima na 
+//tela as seguintes mensagens:
+//1. O primeiro número é igual ao segundo? True/False
+//2. O primeiro número é diferente do segundo? True/False
+//3. O primeiro número é maior que o segundo? True/False
+//4. O primeiro número é menor que o segundo? True/False
+
+/*const numero1 = 10
+const numero2 = 5
+
+console.log('O primeiro número é igual ao segundo?: ', numero1 === numero2)
+
+console.log('O primeiro número é diferente do segundo?: ', numero1!== numero2)
+
+console.log('O primeiro número é maior que o segundo?:', numero1  > numero2 )
+
+console.log('O primeiro número é menor que o segundo?: ',  numero1 < numero2)
+/*
+
+
+
+// ----------------------> Exercicio 3 <---------------------------
+
+Antes de começar, crie 3 variáveis: a, b e c. Atribua os 
+valores true, false e true, respectivamente
+1. Realize a operação: a && b
+2. Realize a operação: b && c
+3. Realize a operação: a && c
+4. Realize a operação: a && b && c
+
+
+
+/* ----------------------> Exercicio 4 <---------------------------
+Antes de começar, crie 3 variáveis: a, b e c. Atribua os 
+valores true, false e true, respectivamente
+a= true
+b = false
+c = true
+1. Realize a operação: a || b // true
+3. Realize a operação: a || c // true 
+4. Realize a operação: a || b || c  //true
+// */
+//  const a = true
+//  const b = false
+//  const c = true
+
+//  console.log('A)', a || b)
+
+
+// ----------------------> Exercicio 5 <---------------------------
+
+// Faça um programa que receba o 
+// nome, ano de nascimento de uma 
+// pessoa e o ano atual e mostre:
+// ● O nome da pessoa com um olá
+// ● A idade dessa pessoa
+// ● Um true ou false que diz se ela é 
+// maior de idade
+// ● Quantos anos ela terá em 2050
+
+ const nome = prompt("Qual é o seu nome?")
+ console.log(nome)
+
+
+
---
+++ semana1/variaveis/template_javascript/index.js
@@ -0,0 +1,31 @@
+// const nome = prompt("Qual é o seu nome?")
+// const idade = prompt("Qual sua idade?")
+// const profissao = prompt("Qual sua profissão?")
+// const religiao = prompt("Qual sua religião?")
+// const familia = prompt("como está sua família?")
+
+// console.log(nome, idade, profissao, religiao, familia)
+
+// const nome = prompt("Qual é o seu nome?")
+// console.log(nome)
+
+// let idade = prompt("Qual sua idade?")
+// console.log(idade)
+
+// idade = 33
+// console.log(idade)
+ 
+// const nome = prompt("Qual seu nome?")
+// const sobreNome = prompt("Qual seu último nome?")
+
+// let idade = prompt("Qual sua idade?")
+// const anoDeNascimento = prompt("Em que ano você nasceu?")
+
+// console.log(nome)
+// console.log(sobreNome)
+// console.log(idade)
+// console.log(anoDeNascimento)
+ const nome = "Gabriel"
+ const idade = 25
+
+console.log ("Oi,meu nome é", nome,",tenho", idade, "anos de idade")
