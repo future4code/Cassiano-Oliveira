fe6a251aaa62b64dd8ad8cd8c80cb1568c13dc0f
Cassiano Moura de Oliveira <cassiano980@gmail.com>
2021-08-19 22:39:02 -0300

    Foram adicionadas algumas pastas

--- semana1/Operadores-Template/index.js
+++ semana1/Operadores-Template/index.js
@@ -43,17 +43,138 @@
 // const resto = 234 % 5
 // console.log("Resultad do módulo:", resto)
 
-let resultado
+// let resultado
 
 // resultado = 1 ===1
 // console.log(`Resultado 1: ${resultado}, é um ${typeof resultado}`)
 
 // resultado = "2" === "2"
-// console.log(`Resultado é : ${resultado}, sendo assim, é ${typeof resultado} `)
+// console.log(`Resultado 2 : ${resultado}, sendo assim, é ${typeof resultado} `)
 
 // resultado = 1===2
-// console.log("Resultado é : ", resultado, ". Sendo assim é um ", typeof resultado)
+// console.log("Resultado 3 : ", resultado, ". Sendo assim é um ", typeof resultado)
 
 // resultado = typeof 2 ===  typeof 10
-// console.log("Comparando tipos de variaveis:", resultado)
+// console.log("Resultado 4:", resultado)
 
+// resultado = 1 !== 2
+// console.log("Resultado 5:", resultado)
+
+// resultado = 1 !== "1"
+// console.log("Resultado 6:", resultado)
+
+// resultado = 1 !== 1
+// console.log("Resultado 7:", resultado)
+
+// function handleArr(ar) {
+//     let sum = 0;
+//     for(const number of ar){
+//         sum +=number
+//     }
+//     if (sum > 0 ) {
+//         return sum
+//     }else {
+//         return "É zero"
+//     }
+// }
+// console.log(handleArr([12, 8, 30]))
+
+// function comparaArr(a, b) {
+//     let pontos = 0
+//     for(let i = 0; i < a.length; i++) {
+//         if(a[i] > b[i]) {
+//             pontos++
+//         }
+//     }
+//     return pontos
+// }
+// console.log(compareArr([1, 2, 2], [1, 1, 1]))
+
+// const pokemons = [
+//     {nome: "Bulbasaur", tipo:"grama"},
+//     {nome: "Bellsprout", tipo: "grama"},
+//     {nome: "Charmander", tipo: "fogo"},
+//     {nome: "Vulpix", tipo: "fogo"},
+//     {nome: "Squirtle", tipo: "água"},
+//     {nome: "Psyduck", tipo: "água"},
+// ]
+
+// const callback = (pokemon, indice, array) => {
+//     if(pokemon.tipo === "grama"){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// const algunsPokemons = pokemons.filter(callback)
+
+// console.log(algunsPokemons)
+
+// let resultado 
+
+// resultado = 1 > 1
+// console.log(resultado)
+
+// resultado = 2 > 3
+// console.log(resultado)
+
+// resultado = 1 >=1
+// console.log(resultado)
+
+// resultado = 1 >= 2
+// console.log(resultado)
+// const vari1 = 10
+// const vari2 = 5
+// let resultado
+
+// resultado = 1 === 1
+// console.log(resultado)
+
+// resultado = 1 !== 1
+// console.log("Respostinha é :", resultado)
+
+// resultado = 1 > 2
+// console.log("Resultado 3 é:", resultado)
+
+// resultado = 2 < 3
+// console.log("Resultado é :", resultado)
+
+// const feliz = true
+// const triste = false
+// const happy = true
+
+// let resultado
+// resultado = feliz && triste
+// console.log("Você está feliz?:", resultado)
+
+// resultado = triste && happy
+// console.log("Você está triste?:", resultado)
+
+// resultado = feliz && happy
+// console.log("Você está faceiro?:", resultado)
+
+// resultado = feliz && triste && happy
+// console.log("Você está alegre?:", resultado)
+
+// const estouEmCasa = true
+// const estouSaindo = true
+// const viajando = true
+
+const a = true
+const b = false
+const c = true
+
+let resultado
+
+resultado = a || b
+console.log(resultado)
+
+resultado = b || c
+console.log(resultado)
+
+resultado = a|| c
+console.log(resultado)
+
+resultado = a || b || c
+console.log(resultado)
